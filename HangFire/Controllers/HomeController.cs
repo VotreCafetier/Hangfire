90a583ac5d2484960dd40da232037b6c26e0f30b
Dany Gauthier <dgauthier@groupemorneau.com>
2023-04-20 12:54:56 -0400

    import from local

---
+++ HangFire/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HangFire.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        private readonly IBackgroundJobClient _backgroundJobs;
+
+        public HomeController(IBackgroundJobClient backgroundJobs)
+        {
+            _backgroundJobs = backgroundJobs;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            _backgroundJobs.Enqueue(() => Console.WriteLine("Hello world from Hangfire!"));
+            return Ok(_backgroundJobs);
+        }
+    }
+}
